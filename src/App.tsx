c335a94d72cfd5b7cda6ce5387ed0e40de7311a1
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-09-29 14:39:32 +0300

    unnecessary import removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./App.css";
 import Snake from "./components/Snake/Snake";
 import Tetris from "./components/Tetris/Tetris";
