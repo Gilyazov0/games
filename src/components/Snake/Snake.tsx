affdf9413240037a8083b57d966de169e113a11c
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-10-01 10:54:28 +0300

    snake refactoring

--- src/components/Snake/Snake.tsx
+++ src/components/Snake/Snake.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { SnakeManipulations as GM } from "../../libs/snake/snakeManipulations";
 import SnakeField from "./SnakeField";
 import { SnakeGameParameters } from "../../dataTypes/snakeDataTypes";
@@ -30,77 +30,46 @@ const Snake: React.FC<{ exitToMenu: Function }> = (props) => {
     currentAction: Actions.MoveLeft,
   });
 
-  function moveFigure(dx: number, dy: number, rotate = false) {
-    setState((prevState: GameState) => {
-      if (prevState.pause) return prevState;
-      const newFigure = GM.getMovedFigure(
-        prevState.figure,
-        dx,
-        dy,
-        prevState.field
-      );
-
-      if (!GM.isValidPosition(prevState.figure, newFigure.figureCoordinates)) {
-        return { ...prevState, gameOver: true };
-      }
-
-      if (GM.isFood(newFigure.figureCoordinates, prevState.field)) {
-        const newField = GM.clearCell(
-          newFigure.figureCoordinates,
+  const moveFigure = useCallback(
+    (dx: number, dy: number, rotate = false) => {
+      setState((prevState: GameState) => {
+        if (prevState.pause) return prevState;
+        const newFigure = GM.getMovedFigure(
+          prevState.figure,
+          dx,
+          dy,
           prevState.field
         );
-        return {
-          ...prevState,
-          figure: newFigure,
-          lastTik: Date.now(),
-          field: GM.putFood(newFigure, 1, newField),
-          score: prevState.score + 1,
-          speed: Math.ceil((prevState.score + 1) / GP.changeSpeedCoef),
-        };
-      } else
-        return {
-          ...prevState,
-          figure: newFigure,
-          lastTik: Date.now(),
-        };
-    });
-  }
 
-  function makeAction(): void {
-    if (state.gameOver) return;
-    switch (action.nextAction) {
-      case Actions.MoveUp:
-        setAction({
-          currentAction: Actions.MoveUp,
-          nextAction: Actions.MoveUp,
-        });
-        moveFigure(0, -1, false);
-        break;
-      case Actions.MoveDown:
-        setAction({
-          currentAction: Actions.MoveDown,
-          nextAction: Actions.MoveDown,
-        });
-        moveFigure(0, 1, false);
-        break;
-      case Actions.MoveLeft:
-        setAction({
-          currentAction: Actions.MoveLeft,
-          nextAction: Actions.MoveLeft,
-        });
-        moveFigure(-1, 0, false);
-        break;
-      case Actions.MoveRight:
-        setAction({
-          currentAction: Actions.MoveRight,
-          nextAction: Actions.MoveRight,
-        });
-        moveFigure(1, 0, false);
-        break;
-      default:
-        break;
-    }
-  }
+        if (
+          !GM.isValidPosition(prevState.figure, newFigure.figureCoordinates)
+        ) {
+          return { ...prevState, gameOver: true };
+        }
+
+        if (GM.isFood(newFigure.figureCoordinates, prevState.field)) {
+          const newField = GM.clearCell(
+            newFigure.figureCoordinates,
+            prevState.field
+          );
+          return {
+            ...prevState,
+            figure: newFigure,
+            lastTik: Date.now(),
+            field: GM.putFood(newFigure, 1, newField),
+            score: prevState.score + 1,
+            speed: Math.ceil((prevState.score + 1) / GP.changeSpeedCoef),
+          };
+        } else
+          return {
+            ...prevState,
+            figure: newFigure,
+            lastTik: Date.now(),
+          };
+      });
+    },
+    [GP.changeSpeedCoef]
+  );
 
   function togglePause() {
     setState((prevState) => {
@@ -108,108 +77,144 @@ const Snake: React.FC<{ exitToMenu: Function }> = (props) => {
     });
   }
 
-  function handleUserInput(input: Actions): void {
-    switch (input) {
-      case Actions.MoveUp:
-        if (
-          action.currentAction === Actions.MoveLeft ||
-          action.currentAction === Actions.MoveRight
-        ) {
-          setAction((prevAction) => {
-            return { ...prevAction, nextAction: Actions.MoveUp };
-          });
-        }
-        break;
-      case Actions.MoveDown:
-        if (
-          action.currentAction === Actions.MoveLeft ||
-          action.currentAction === Actions.MoveRight
-        ) {
-          setAction((prevAction) => {
-            return { ...prevAction, nextAction: Actions.MoveDown };
-          });
-        }
-        break;
-      case Actions.MoveLeft:
-        if (
-          action.currentAction === Actions.MoveUp ||
-          action.currentAction === Actions.MoveDown
-        ) {
-          setAction((prevAction) => {
-            return { ...prevAction, nextAction: Actions.MoveLeft };
-          });
-        }
-        break;
-      case Actions.MoveRight:
-        if (
-          action.currentAction === Actions.MoveUp ||
-          action.currentAction === Actions.MoveDown
-        ) {
-          setAction((prevAction) => {
-            return { ...prevAction, nextAction: Actions.MoveRight };
-          });
-        }
-        break;
-      case Actions.Pause:
-        togglePause();
-        break;
-    }
-  }
-
-  function handleUserKeyPress(event: KeyboardEvent): void {
-    switch (event.key) {
-      case "ArrowUp":
-        handleUserInput(Actions.MoveUp);
-        break;
-      case "ArrowDown":
-        handleUserInput(Actions.MoveDown);
-        break;
-      case "ArrowLeft":
-        handleUserInput(Actions.MoveLeft);
-        break;
-      case "ArrowRight":
-        handleUserInput(Actions.MoveRight);
-        break;
-      case " ":
-        handleUserInput(Actions.Pause);
-        break;
-      default:
-        break;
-    }
-  }
-
-  function handleTouchStart(event: TouchEvent): void {
-    const x = event.changedTouches[0].clientX;
-    const y = event.changedTouches[0].clientY;
-    event.stopPropagation();
-    if (y > window.innerHeight * (1 - GP.touchZoneSizeY)) {
-      handleUserInput(Actions.MoveDown);
-    } else if (y < window.innerHeight * GP.touchZoneSizeY) {
-      handleUserInput(Actions.MoveUp);
-    } else if (x < window.innerWidth * GP.touchZoneSizeX) {
-      handleUserInput(Actions.MoveLeft);
-    } else if (x > window.innerWidth * (1 - GP.touchZoneSizeX)) {
-      handleUserInput(Actions.MoveRight);
-    } else handleUserInput(Actions.Pause);
-  }
+  const handleUserInput = useCallback(
+    (input: Actions): void => {
+      switch (input) {
+        case Actions.MoveUp:
+          if (
+            action.currentAction === Actions.MoveLeft ||
+            action.currentAction === Actions.MoveRight
+          ) {
+            setAction((prevAction) => {
+              return { ...prevAction, nextAction: Actions.MoveUp };
+            });
+          }
+          break;
+        case Actions.MoveDown:
+          if (
+            action.currentAction === Actions.MoveLeft ||
+            action.currentAction === Actions.MoveRight
+          ) {
+            setAction((prevAction) => {
+              return { ...prevAction, nextAction: Actions.MoveDown };
+            });
+          }
+          break;
+        case Actions.MoveLeft:
+          if (
+            action.currentAction === Actions.MoveUp ||
+            action.currentAction === Actions.MoveDown
+          ) {
+            setAction((prevAction) => {
+              return { ...prevAction, nextAction: Actions.MoveLeft };
+            });
+          }
+          break;
+        case Actions.MoveRight:
+          if (
+            action.currentAction === Actions.MoveUp ||
+            action.currentAction === Actions.MoveDown
+          ) {
+            setAction((prevAction) => {
+              return { ...prevAction, nextAction: Actions.MoveRight };
+            });
+          }
+          break;
+        case Actions.Pause:
+          togglePause();
+          break;
+      }
+    },
+    [action.currentAction]
+  );
 
   React.useEffect(() => {
+    function handleTouchStart(event: TouchEvent): void {
+      const x = event.changedTouches[0].clientX;
+      const y = event.changedTouches[0].clientY;
+      event.stopPropagation();
+      if (y > window.innerHeight * (1 - GP.touchZoneSizeY)) {
+        handleUserInput(Actions.MoveDown);
+      } else if (y < window.innerHeight * GP.touchZoneSizeY) {
+        handleUserInput(Actions.MoveUp);
+      } else if (x < window.innerWidth * GP.touchZoneSizeX) {
+        handleUserInput(Actions.MoveLeft);
+      } else if (x > window.innerWidth * (1 - GP.touchZoneSizeX)) {
+        handleUserInput(Actions.MoveRight);
+      } else handleUserInput(Actions.Pause);
+    }
+
     window.addEventListener("touchstart", handleTouchStart);
     return () => {
       window.removeEventListener("touchstart", handleTouchStart);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [action]);
+  }, [GP.touchZoneSizeX, GP.touchZoneSizeY, action, handleUserInput]);
 
   React.useEffect(() => {
+    function handleUserKeyPress(event: KeyboardEvent): void {
+      switch (event.key) {
+        case "ArrowUp":
+          handleUserInput(Actions.MoveUp);
+          break;
+        case "ArrowDown":
+          handleUserInput(Actions.MoveDown);
+          break;
+        case "ArrowLeft":
+          handleUserInput(Actions.MoveLeft);
+          break;
+        case "ArrowRight":
+          handleUserInput(Actions.MoveRight);
+          break;
+        case " ":
+          handleUserInput(Actions.Pause);
+          break;
+        default:
+          break;
+      }
+    }
     window.addEventListener("keydown", handleUserKeyPress);
     return () => {
       window.removeEventListener("keydown", handleUserKeyPress);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [action]);
+  }, [action, handleUserInput]);
 
   React.useEffect(() => {
+    function makeAction(): void {
+      if (state.gameOver) return;
+      switch (action.nextAction) {
+        case Actions.MoveUp:
+          setAction({
+            currentAction: Actions.MoveUp,
+            nextAction: Actions.MoveUp,
+          });
+          moveFigure(0, -1, false);
+          break;
+        case Actions.MoveDown:
+          setAction({
+            currentAction: Actions.MoveDown,
+            nextAction: Actions.MoveDown,
+          });
+          moveFigure(0, 1, false);
+          break;
+        case Actions.MoveLeft:
+          setAction({
+            currentAction: Actions.MoveLeft,
+            nextAction: Actions.MoveLeft,
+          });
+          moveFigure(-1, 0, false);
+          break;
+        case Actions.MoveRight:
+          setAction({
+            currentAction: Actions.MoveRight,
+            nextAction: Actions.MoveRight,
+          });
+          moveFigure(1, 0, false);
+          break;
+        default:
+          break;
+      }
+    }
+
     let interval = window.setInterval(
       () => makeAction(),
       state.lastTik - Date.now() + GP.baseSpeed / state.speed
@@ -217,8 +222,14 @@ const Snake: React.FC<{ exitToMenu: Function }> = (props) => {
     return () => {
       window.clearInterval(interval);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [action, state.speed, state.lastTik]);
+  }, [
+    action,
+    state.speed,
+    state.lastTik,
+    GP.baseSpeed,
+    state.gameOver,
+    moveFigure,
+  ]);
 
   return (
     <SnakeField
