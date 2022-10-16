f5b4e6f75b6e11caedad12efacf1c5b629fbdc90
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-10-16 15:28:04 +0300

    adjustmentToScreenSize

--- src/libs/adjustmentToScreen.tsx
+++ src/libs/adjustmentToScreen.tsx
@@ -4,20 +4,26 @@ export function adjustmentToScreenSize(
   rowsCount: number,
   colsCount: number
 ): [number, Direction] {
-  const maxWidth = Math.min(window.innerWidth, window.screen.width);
-  const maxHight = Math.min(window.innerHeight, window.screen.height);
-  const margin = 15;
+  let maxWidth = Math.min(window.innerWidth, window.screen.width);
+  let maxHight = Math.min(window.innerHeight, window.screen.height);
+  const margin = 210;
 
   let cellSizeByRows = (maxHight - margin) / rowsCount;
   let cellSizeByCols = (maxWidth - margin) / colsCount;
 
   const direction: Direction =
     cellSizeByRows > cellSizeByCols ? Direction.column : Direction.row;
-  const addCols = direction === Direction.column ? 0 : 3;
-  const addRows = direction === Direction.row ? 0 : 4;
+  // const addCols = direction === Direction.column ? 0 : 3;
+  // const addRows = direction === Direction.row ? 0 : 4;
 
-  cellSizeByRows = (maxHight - margin) / (rowsCount + addRows);
-  cellSizeByCols = (maxWidth - margin) / (colsCount + addCols);
+  if (direction === Direction.column) {
+    maxHight -= 210;
+  } else {
+    maxWidth -= 210;
+  }
+
+  cellSizeByRows = maxHight / rowsCount;
+  cellSizeByCols = maxWidth / colsCount;
 
   let cellSize = Math.min(cellSizeByRows, cellSizeByCols);
   cellSize = Math.floor(cellSize) - 2;
